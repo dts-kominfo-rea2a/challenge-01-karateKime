8712d646cf86f7a2e015531df0d8f99cac617ad2
karateKime <iqbaljka@gmail.com>
2022-05-29 18:55:30 +0700

    feat:Tugas Data Structure JavaScript Muhammad Iqbal

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
 
-// TODO: Masukkan hasil akhir dari kedua user di sini
+// const firstUser = {};
+// const secondUser = {};
+
+
+colorMonica = ['Yellow, Pink, White, Purple'];
+colorWendy = ['Blue, Black, Grey'];
+
+educationMonica = {
+ SD : 'name: SD 01, city: Jakarta, graduate: 2016',
+ SMP : 'name: SMP 02, city: Jakarta, graduate: 2019',
+ SMA : 'name: SMA 03, city: Tangerang'
+};
+
+educationWendy = {
+    SD : 'name: SD 02, city: Jakarta, graduate: 2010',
+    SMP : 'name: SMP 03, city: Bogor, graduate: 2013',
+    SMA : 'name: SMA 01, city: Surabaya, graduate: 2016',
+    UNIV : 'name: Universitas Maju, city: Tangerang'
+   };
+
+favRestoMonica = ['Bento, Sushi, Pancake, Eggy, Tempura, Bento, Eggy, Padang, Tteok, Sushi, Sushi'];
+favRestoWendy = ['Tempura, Bento, Sushi, Pancake, Padang, Katsu, Geprek, Pancake, Eggy'];
+
+userMonica = {
+    name : 'Monica',
+    gender : 'Female',
+    age : 17,
+    email : 'monica@dingdong.com',
+    favoriteColor : colorMonica[0],
+    isHavePet : 'Yes',
+    education : [educationMonica.SD, educationMonica.SMP, educationMonica.SMA],
+    favoriteRestaurant : favRestoMonica[0]
+};
+
+userWendy = {
+    name : 'Wendy',
+    gender : 'Male',
+    age : 23,
+    email : 'wendy@dingdong.com@dingdong.com',
+    favoriteColor : colorWendy[0],
+    isHavePet : 'Yes',
+    education : [educationWendy.SD, educationWendy.SMP, educationWendy.SMA, educationWendy.UNIV],
+    favoriteRestaurant : favRestoWendy[0]
+};
+
+// // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+
+users.push(userMonica);
+users.push(userWendy);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
@@ -18,3 +64,5 @@ main();
 module.exports = {
     users
 };
+
+// console.log(users);
